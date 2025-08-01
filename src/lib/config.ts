106f0fb99a095a5c28fddc137cdd368c85e89d4a
CarrotDLaw <carrotdlaw@gmail.com>
2025-08-02 00:46:59 +0800

    feat: ✨ updated range to start from one week ago

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,22 +1,24 @@
-import { ICalEventBusyStatus } from 'ical-generator';
-import type { QueryDatabaseParameters } from '@notionhq/client/build/src/api-endpoints';
+import { ICalEventBusyStatus } from "ical-generator";
+import type { QueryDatabaseParameters } from "@notionhq/client/build/src/api-endpoints";
 
-const today = new Date().toISOString().split('T')[0];
+const one_week_ago = new Date(new Date().setDate(new Date().getDate() - 7))
+  .toISOString()
+  .split("T")[0];
 
 export default {
-	filter: {
-		property: 'Date',
-        date: { on_or_after: today }
-	},
-	dateProperty: 'Date',
-	titleProperty: 'Task',
-	locationProperty: 'Location',
+  filter: {
+    property: "Date",
+    date: { on_or_after: one_week_ago },
+  },
+  dateProperty: "Date",
+  titleProperty: "Task",
+  locationProperty: "Location",
 
-	busy: ICalEventBusyStatus.FREE
+  busy: ICalEventBusyStatus.FREE,
 } as {
-	filter: Readonly<QueryDatabaseParameters['filter']>;
-	dateProperty: Readonly<string>;
-	titleProperty: Readonly<string>;
-	locationProperty: Readonly<string>;
-	busy: Readonly<ICalEventBusyStatus>;
+  filter: Readonly<QueryDatabaseParameters["filter"]>;
+  dateProperty: Readonly<string>;
+  titleProperty: Readonly<string>;
+  locationProperty: Readonly<string>;
+  busy: Readonly<ICalEventBusyStatus>;
 };
